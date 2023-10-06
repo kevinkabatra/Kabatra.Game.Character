b8a4473d6c4a513189a4b9b739087db09509b929
Kevin Kabatra <kevinkabatra@gmail.com>
2023-10-05 23:38:24 -0400

    Added test for Lightfoot.
    The benefit of the generic test approach is that the test itself is only defined once. This benefit will be very apparent as new features are added and require testing for each race. Each race must also define the expected properties or their tests will fail, so it also enforces that all of the data for each race is tested.

---
+++ Kabatra.Game.Character/Kabatra.Game.Character.Tests/Races/Halflings/LightfootTests.cs
@@ -0,0 +1,32 @@
+﻿namespace Kabatra.Game.Character.Tests.Races.Halflings
+{
+    using Kabatra.Game.Character.Abilities;
+    using Kabatra.Game.Character.Languages;
+    using Kabatra.Game.Character.Races.Halflings;
+    using Kabatra.Game.Character.Sizes;
+    using Kabatra.Game.Character.Tests.Races.Data.Halflings;
+
+    public class LightfootTests : RaceTests<Lightfoot, GenericLightfoot>
+    {
+        public override float ExpectedSpeedInFeet { get => 25f; }
+        public override SizeCategory ExpectedSizeCategory { get => SizeCategory.Small; }
+
+        public override List<Language> ExpectedLanguages
+        {
+            get => new()
+            {
+                Language.Common,
+                Language.Halfling
+            };
+        }
+
+        public override List<AbilityScoreIncrease> ExpectedAbilityScoreIncrease
+        {
+            get => new()
+            {
+                new(Ability.Dexterity, 2),
+                new(Ability.Charisma, 1),
+            };
+        }
+    }
+}
